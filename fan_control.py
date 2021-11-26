1f18cf97633e843836cc493736b9c21a82d2052a
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-26 11:07:25 +0100

    Simplify the dead_band logic by an OFF_TEMP constant

--- fan_control.py
+++ fan_control.py
@@ -8,15 +8,14 @@ FAN_PIN = 18            # BCM pin used to drive PWM fan
 WAIT_TIME = 1           # [s] Time to wait between each refresh
 PWM_FREQ = 25           # [Hz] 25Hz for Noctua PWM control
 
-MIN_TEMP = 45
-MIN_TEMP_DEAD_BAND = 5
-MAX_TEMP = 70
+OFF_TEMP = 40           # [°C] temperature below which to stop the fan
+MIN_TEMP = 45           # [°C] temperature above which to start the fan
+MAX_TEMP = 70           # [°C] temperature at which to operate at max fan speed
 FAN_LOW = 1
 FAN_HIGH = 100
 FAN_OFF = 0
 FAN_MAX = 100
-
-outside_dead_band_higher = True
+FAN_GAIN = float(FAN_HIGH - FAN_LOW) / float(MAX_TEMP - MIN_TEMP)
 
 
 def getCpuTemperature():
@@ -24,22 +23,13 @@ def getCpuTemperature():
         return float(f.read()) / 1000
 
 
-def handleFanSpeed(fan, temperature, outside_dead_band_higher):
+def handleFanSpeed(fan, temperature):
+    if temperature > MIN_TEMP:
+        delta = min(temperature, MAX_TEMP) - MIN_TEMP
+        fan.start(FAN_LOW + delta * FAN_GAIN)
 
-    if not outside_dead_band_higher:
+    elif temperature < OFF_TEMP:
         fan.start(FAN_OFF)
-
-    elif outside_dead_band_higher and temperature < MAX_TEMP:
-        step = float(FAN_HIGH - FAN_LOW)/float(MAX_TEMP - MIN_TEMP)
-        temperature -= MIN_TEMP
-        fan.start(FAN_LOW + (round(temperature) * step))
-
-    elif temperature > MAX_TEMP:
-        fan.start(FAN_MAX)
-
-
-def handleDeadZone(temperature):
-    return temperature > (MIN_TEMP + MIN_TEMP_DEAD_BAND/2)
 
 
 try:
@@ -49,9 +39,7 @@ try:
     GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
     fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
     while True:
-        temp = getCpuTemperature()
-        outside_dead_band_higher = handleDeadZone(temp)
-        handleFanSpeed(fan, temp, outside_dead_band_higher)
+        handleFanSpeed(fan, getCpuTemperature())
         time.sleep(WAIT_TIME)
 
 except KeyboardInterrupt:
